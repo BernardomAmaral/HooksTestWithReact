a5aacffc2138d75caa6a69f5c9c5adc9cc2b7e67
Your Name <bernardoamaralmarinho@gmail.com>
2020-05-17 03:24:22 -0300

    Adding favorite repositorie using GitHub api to listing my repositories in a viewer with Hooks

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import React, { useState, useEffect } from 'react';
 
+// exporting in function form
 export default function App() {
 
+// setting the State using Hooks useState
   const [repositories, setRepositories] = useState([]);
 
+// Using Hooks useEffect to define state conditions
   useEffect( async () => {
-    const response = await fetch('https://api.github.com/users/diego3g/repos');
+    const response = await fetch('https://api.github.com/users/BernardomAmaral/repos');
     const data = await response.json();
       setRepositories(data)
   },[])
+
+//Favorite function
+  function handleFavorite(id){
+    const newRepositories = repositories.map( repo => {
+      return repo.id === id ? {...repo, favorite: true} : repo
+    });
+
+    setRepositories(newRepositories);
+  }
 
   return ( 
      
      <ul>
        { repositories.map( repo => (
-          <li key={repo.id}>{repo.name}</li>
+          <li key={repo.id}>
+            {repo.name}
+            {repo.favorite && <span>(Favorito)</span> }
+            <button onClick={ () => handleFavorite(repo.id) }>
+              Favoritar
+            </button>
+          </li>
           ))}
      </ul>
 
