3f8aac3868196de3f1f700e30711049773fe4708
Your Name <bernardoamaralmarinho@gmail.com>
2020-05-17 03:48:22 -0300

    Implementing a visual return to the favorites lenght

--- src/App.js
+++ src/App.js
@@ -6,17 +6,25 @@ export default function App() {
 // setting the State using Hooks useState
   const [repositories, setRepositories] = useState([]);
 
+
 // Using Hooks useEffect to define state conditions
   useEffect( async () => {
     const response = await fetch('https://api.github.com/users/BernardomAmaral/repos');
     const data = await response.json();
       setRepositories(data)
-  },[])
+  },[]);
+//Implementing a visual return to the favorites lenght
+  useEffect(() => {
+    const filterFavorites = repositories.filter(repo => repo.favorite);
+
+    document.title = `Você tem ${filterFavorites.length} favoritos`
+  }, [repositories])
+
 
 //Favorite function
   function handleFavorite(id){
     const newRepositories = repositories.map( repo => {
-      return repo.id === id ? {...repo, favorite: true} : repo
+      return repo.id === id ? {...repo, favorite: !repo.favorite } : repo
     });
 
     setRepositories(newRepositories);
