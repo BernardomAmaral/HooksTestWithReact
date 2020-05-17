beba1bb22d49b197cabac6844da7651b2efcdd03
Your Name <bernardoamaralmarinho@gmail.com>
2020-05-17 02:43:16 -0300

    App.Js Atualizado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function App() {
-  return (
-    <h1>Hello World</h1>
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect( async () => {
+    const response = await fetch('https://api.github.com/users/diego3g/repos');
+    const data = await response.json();
+      setRepositories(data)
+  },[])
+
+  return ( 
+     
+     <ul>
+       { repositories.map( repo => (
+          <li key={repo.id}>{repo.name}</li>
+          ))}
+     </ul>
+
+   );
  
-  );
+
 }
 
 
